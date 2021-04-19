aedf40bb4ce218cc85cbbb45069cd3272992bac5
EunbInn <dsino@naver.com>
2021-04-19 11:22:55 +0900

    change file Path

--- TicketProject/src/Amusementpark/SetData.java
+++ TicketProject/src/Amusementpark/SetData.java
@@ -1,11 +1,15 @@
 package Amusementpark;
 
 public class SetData {
+
 	final static int[] dayPrice = { 0, 44000, 47000, 56000, 44000 };// 유아 소인 청소년 대인 경로
 	final static int[] nightPrice = { 0, 37000, 40000, 46000, 37000 };// 유아 소인 청소년 대인 경로
 	final static double[] discount = { 1, 0.6, 0.5, 0.2, 0.15 };// 장애인, 국가유공자, 다자녀, 임산부
 	final static String[] ageName = { "유아", "소인", "청소년", "대인", "경로" };
 	final static String[] preName = { "없음", "장애인", "국가유공자", "다자녀", "임산부" };
+	
+	final static String day = "주간권";
+	final static String night = "야간권";
 
 	final static int childMin = 3;// 나이대별 민맥스 상수화
 	final static int childMax = 12;
@@ -27,5 +31,5 @@ public class SetData {
 
 	final static int ticketMax = 10; // 티켓 맥스
 
-	final static String filePath = "C:\\project\\amuse.csv"; // 파일경로
+	final static String filePath = ".\\amuse.csv"; // 파일경로
 }
--- TicketProject/src/ReportSystem/ConstValue.java
+++ TicketProject/src/ReportSystem/ConstValue.java
@@ -2,29 +2,29 @@ package ReportSystem;
 
 public class ConstValue {
 
-	final static String[] ageName = {"유아","소인","청소년","대인","경로"};
-	final static String[] preName = {"없음","장애인","국가유공자","다자녀","임산부"};
-	final static String[] dayNight = {"주간권","야간권"};
+	public final static String[] ageName = {"유아","소인","청소년","대인","경로"};
+	public final static String[] preName = {"없음","장애인","국가유공자","다자녀","임산부"};
+	public final static String[] dayNight = {"주간권","야간권"};
 	
-	final static int day = 1;
-	final static int night = 2;
+	public final static int day = 1;
+	public final static int night = 2;
 	
-	final static int baby = 1;//나이대별 그룹 넘버 상수화
-	final static int child = 2;
-	final static int teen = 3;
-	final static int adult = 4;
-	final static int old = 5;
+	public final static int baby = 1;//나이대별 그룹 넘버 상수화
+	public final static int child = 2;
+	public final static int teen = 3;
+	public final static int adult = 4;
+	public final static int old = 5;
 	
-	final static int none = 1; //우대 종류 상수화
-	final static int disabled = 2;
-	final static int national = 3;
-	final static int multiChild = 4;
-	final static int pregnant = 5;
+	public final static int none = 1; //우대 종류 상수화
+	public final static int disabled = 2;
+	public final static int national = 3;
+	public final static int multiChild = 4;
+	public final static int pregnant = 5;
 	
-	final static String filePath = "C:\\project\\amuse.csv";
-	final static String fileWritePath_1 = "C:\\project\\kindReport.csv";
-	final static String fileWritePath_2 = "C:\\project\\DailyReport.csv";
-	final static String fileWritePath_3 = "C:\\project\\prefReport.csv";
+	public final static String filePath = ".\\amuse.csv";
+	public final static String fileWritePath_1 = ".\\kindReport.csv";
+	public final static String fileWritePath_2 = ".\\DailyReport.csv";
+	public final static String fileWritePath_3 = ".\\prefReport.csv";
 	
 	
 
